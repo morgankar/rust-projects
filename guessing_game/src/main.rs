d4b837308dfd6c9d984df8f9ded889d79d16474c
Rohan Kar <realrohankar03@gmail.com>
2024-05-01 08:43:10 +0530

    wip

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;  // importing input/output library
+
+fn main(){
+    println!("******Guess the number!******");
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+    io::stdin()
+
+}
